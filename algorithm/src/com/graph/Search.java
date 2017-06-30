fa3526d33b310632b2c7b36a80da4ba4bff4b7aa
zhanglong <jianglong161@163.com>
2017-06-30 09:52:04 +0800

    图处理算法的API

---
+++ algorithm/src/com/graph/Search.java
@@ -0,0 +1,32 @@
+package com.graph;
+/**
+ * 图处理算法的额API
+ * @author Still2Almost
+ *
+ */
+public class Search {
+	private Graph graph;
+	/**
+	 * 找到和起点s连通的所有顶点
+	 * @param graph
+	 * @param s
+	 */
+	public Search(Graph graph, int s) {
+		
+	}
+	/**
+	 * v和s是连通的么
+	 * @param v
+	 * @return
+	 */
+	public boolean marked(int v){
+		return false;
+		
+	}
+	/**
+	 * 与s连通的顶点个数
+	 * @return
+	 */
+	public int count(){
+		return 0;}
+}
