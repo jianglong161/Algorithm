b6cf7fe1a9af9a8726cb63248cef553bbd81854a
zhanglong <jianglong161@163.com>
2017-07-04 10:51:12 +0800

    符号图的数据类型

---
+++ algorithm/src/com/graph/In.java
@@ -0,0 +1,19 @@
+package com.graph;
+
+public class In {
+
+	public In(String stream) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean hasNextLine() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String readLine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
