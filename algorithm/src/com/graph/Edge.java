66c176f7dfb9e38b317b82bdeb456e8d65df8aab
zhanglong <jianglong161@163.com>
2017-07-07 10:24:55 +0800

    带权重的边的数据类型

---
+++ algorithm/src/com/graph/Edge.java
@@ -0,0 +1,57 @@
+package com.graph;
+
+import java.util.Iterator;
+
+import javax.management.RuntimeErrorException;
+import javax.print.attribute.standard.MediaSize.Other;
+
+/**
+ * 带权重的边的数据类型
+ * @author Still2Almost
+ *
+ */
+public class Edge implements Iterable<Edge>{
+	private final int v;//顶点之一
+	private final int w;//另一个顶点
+	private final double weight;	//边的权重
+	public Edge(int v,int w,double weight) {
+		this.v = v;
+		this.w = w;
+		this.weight = weight;
+	}
+	public double weight(){
+		return weight;
+	}
+	public int either(){
+		return v;
+	}
+	public int Other(int vertex){
+		if(vertex == v)
+			return w;
+		else if(vertex == w)
+			return v;
+		else{
+			throw new RuntimeException( "Inconsitant edge");
+		}
+	}
+	public int comparetTo(Edge that){
+		if(this.weight() < that.weight())
+			return -1;
+		else if (this.weight() > that.weight()) {
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+	public String toString(){
+		return String.format("%d-%d %.2f",v,w,weight);
+	}
+	
+	@Override
+	public Iterator<Edge> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
